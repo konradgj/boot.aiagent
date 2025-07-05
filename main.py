3f495ecc26c94b6966ec222d8663bb19b47ec6eb
konradgj <konrad.gjerstad@gmail.com>
2025-07-05 21:46:37 +0200

    added ruff for linting and formating

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+
 def main():
     verbose = "--verbose" in sys.argv
     args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
-    
+
     if not args:
-        print('No prompt provided. Exiting')
+        print("No prompt provided. Exiting")
         sys.exit(1)
 
     load_dotenv()
@@ -18,17 +19,16 @@ def main():
 
     model = "gemini-2.0-flash-001"
     prompt = " ".join(args)
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=prompt)])
-    ]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    response = client.models.generate_content(model=model, contents=messages)   
+    response = client.models.generate_content(model=model, contents=messages)
 
     print(response.text)
     if verbose:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
